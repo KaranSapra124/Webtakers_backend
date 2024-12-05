9be9e793cf79ce76af8f0a47bdef3ef33e706da1
Karan sapra <88654249+KaranSapra124@users.noreply.github.com>
2024-12-05 13:46:26 +0530

    Update app.js

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.use(express.json());
 
 app.use(
       cors({
-        origin: "http://127.0.0.1:5500",
+        origin: "https://webtakersitsolutions.com",
         credentials: true,
       })
     );
